2069b4dbc062727c5c83c8e9be45f11bd48debdf
hangle02 <lethuhang.cv@gmail.com>
2026-02-25 21:49:58 +0700

    meo

---
+++ Sudoku_hw4.java
@@ -0,0 +1,13 @@
+public class Sudoku_hw4 {
+    public static int[] find_possible_values(int[] matrix, int x, int y){
+
+    }
+
+    public static void solve(int[][] matrix){
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
