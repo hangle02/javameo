ee939533decc880d6a961e52593338efb2a1db84
hangle02 <lethuhang.cv@gmail.com>
2026-03-03 13:54:35 +0700

    meo

--- Sudoku_hw4.java
+++ Sudoku_hw4.java
@@ -1,6 +1,7 @@
+//package com.gradescope.cs201;
 public class Sudoku_hw4 {
     public static int[] find_possible_values(int[] matrix, int x, int y){
-
+        return matrix;
     }
 
     public static void solve(int[][] matrix){
