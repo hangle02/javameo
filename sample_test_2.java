c84c1e280275a77e8000fd48a5163692862b5a3b
hangle02 <lethuhang.cv@gmail.com>
2026-04-07 13:51:00 +0700

    meo

---
+++ sample_test_2.java
@@ -0,0 +1,18 @@
+class TreeNode {
+    public int data;
+    public TreeNode left, right;
+    public TreeNode (int data_, TreeNode left_, TreeNode right_) {
+        this.data = data_;
+        this.left = left_;
+        this.right = right_;
+    }
+}
+public class sample_test_2 {
+    public static int get_median_element(TreeNode bst_root){
+
+        return 0;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
