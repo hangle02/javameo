e425e90c11fa8b3ecddc1db928a0e97fa8c75311
hangle02 <lethuhang.cv@gmail.com>
2026-05-06 15:40:21 +0700

    meo

--- Graph_hw9.java
+++ Graph_hw9.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.LinkedList;
 interface Hw9_interface {
     public boolean add_node(int x);
@@ -7,11 +8,35 @@ interface Hw9_interface {
     public LinkedList<LinkedList<Integer>> find_bridges(); 
 }
 public class Graph_hw9 implements Hw9_interface {
-    public boolean add_node(int x){ return false;}
+    HashMap<Integer, LinkedList<Integer>> adj_list;
+    public Graph_hw9(){
+        this.adj_list = new HashMap<>();
+    }
 
-    public boolean add_edge(int x, int y){return false;}
+    public boolean add_node(int x){
+        if(adj_list.keySet().contains(x)){
+            return false;
+        }
+        adj_list.putIfAbsent(x, new LinkedList<>()); 
+        return true;
+    }
 
-    public LinkedList<LinkedList<Integer>> find_components(){return null;} 
+    public boolean add_edge(int x, int y){
+        if(adj_list.containsKey(y) && adj_list.containsKey(x) && !adj_list.get(x).contains(y)){
+            adj_list.get(x).add(y);
+            return true;
+        }
+        return false;
+    }
+
+    public LinkedList<LinkedList<Integer>> find_components(){
+        LinkedList<LinkedList<Integer>> components = new LinkedList<>();
+        HashMap<Integer, Boolean> visited = new HashMap<>();
+        for(int node: adj_list.keySet()){
+            
+        }
+        return null;
+    } 
 
     public int find_diameter_length(){return 0;}
 
